bcf67fa68cef9904a3e8d12f7b2826ce49d154ac
fyzz <110374089+keagancoding@users.noreply.github.com>
2024-02-11 20:23:37 -0800

    removed mobile popup on gallery pages and put it only on mint page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,10 +4,12 @@ import ConnectButton from "@/components/ConnectButton";
 import Link from "next/link";
 import { MinterProvider } from "@/components/contexts/MinterContext";
 import CollectionButton from "@/components/CollectionButton";
+import MobileModal from "@/components/layout/MobileModal";
 
 export default function Page() {
   return (
     <div className="gap-3 flex flex-1 flex-col px-6 w-full max-w-sm h-full">
+      <MobileModal />
       <MinterProvider>
         <Frame />
         <MintDisplay />
